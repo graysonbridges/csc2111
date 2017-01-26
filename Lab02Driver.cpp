02ee3888e0636fd3973e13d99822a24d0430030b
grayson <graysonbridges@gmail.com>
2017-01-26 14:19:54 -0600

    merge error

--- Lab02Driver.cpp
+++ Lab02Driver.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 int main()
 {
-   ReadFile* rf = new ReadFile("cds.txt");//slcaneer kdiff3 necessary comment //testing, testing
+   ReadFile* rf = new ReadFile("cds.txt");//merge error
    WriteFile* wf = new WriteFile("out.txt");
 
    while(!rf->eof())
