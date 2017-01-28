fb1e781643a8b7af2eff69852f7dfe93c72015dd
Samantha Caneer <slcaneer42@students.tntech.edu>
2017-01-28 13:02:45 -0600

    SCaneer edited 1.28.17, 1:02 pm

--- WriteFile.cpp
+++ WriteFile.cpp
@@ -1,33 +1,31 @@
 #include "WriteFile.h"
 #include <sstream>
 
-WriteFile* createWriteFile(const char* file_name)
+WriteFile::WriteFile(const char* file_name);
 {
-   WriteFile* wf = new WriteFile;
-   wf->output_file.open(file_name);
-   wf->closed = false;
-   return wf;
+   output_file.open(file_name);
+   writeLine();
+   closed = false;
 }
 
-void destroyWriteFile(WriteFile* wf)
+WriteFile::~WriteFile()
 {
-   close(wf);
-   delete wf;
+  
 }
 
-void close(WriteFile* wf)
+void close()
 {
-   if (!wf->closed)
+   if (!this->closed)
    {
-      wf->output_file.close();
-      wf->closed = true;
+      this->output_file.close();
+      this->closed = true;
    }
 }
 
-void writeLine(WriteFile* wf, String* line)
+void writeLine(String* line)
 {
-   if (!wf->closed && line->length() > 0)
+   if (!this->closed && line->length() > 0)
    {
-      wf->output_file << line->getText() << endl;
+      this->output_file << line->getText() << endl;
    }
 }
